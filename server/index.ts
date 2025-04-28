8a8091d22b1800e86606ae9245b2f33e27200672
Pericle Purple AZ <c.corsi99@gmail.com>
2025-04-28 13:54:22 +0000

    Restored to '1e16401636c02317942fab958fb1f9cc2ef6287b'
    Replit-Restored-To: 1e16401636c02317942fab958fb1f9cc2ef6287b

--- server/index.ts
+++ server/index.ts
@@ -1,15 +1,10 @@
 import express, { type Request, Response, NextFunction } from "express";
-import cors from 'cors';
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
 
 const app = express();
-app.use(cors({
-  origin: true,
-  credentials: true
-}));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
   const start = Date.now();
